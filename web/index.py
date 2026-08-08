fe925ca87a07e45b52d2129003d1a762b2a786e7
toochewie <sheepboy610@gmail.com>
2026-08-08 12:18:18 -0400

    Worship Edit Feature - Created an override link (worship/edit) that allows changing of team rosters afterwards

--- web/index.py
+++ web/index.py
@@ -101,11 +101,13 @@ def profile():
 
 # -------- Worship Pages ---------
 @app.route("/worship")
+@app.route("/worship/edit")
 def worship_home():
 	sundays = Tools.allsundays()
 	worship = Utils.worship_list()
 	worship = [{'date': x, 'worship': next((y for y in worship if y['date'] == x), -1)} for x in sundays[1]]
-	return render_template('worship/worship.html', worship=worship, sundays=sundays)
+	is_edit_route = request.path.rstrip('/').endswith('/worship/edit')
+	return render_template('worship/worship.html', worship=worship, sundays=sundays, is_edit_route=is_edit_route)
 
 @app.route("/worship/<id>")
 @app.route("/worship/<id>/<tab>")
